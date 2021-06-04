6521666f0c792c671f28077ee0039bf61fa93149
KValexander <ewoase@gmail.com>
2021-06-04 20:05:47 +0300

    Three prototype

---
+++ 3. Js prototype with canvas v2/data/script.js
@@ -0,0 +1,25 @@
+// Когда загрузиться страница
+window.onload = function() {
+
+	// Отключение контекстного меню
+	document.querySelector("#canvas").oncontextmenu = function () { return false };
+
+	// Создание экземпляра класса игры
+	// let game = new Game;
+
+	// Вызов игрового цикла || 60 fps
+	window.requestAnimationFrame(gameLoop);
+
+	// Функция игрового цикла
+	function gameLoop() {
+		// Вызов метода обновления данных игры
+		// game.update();
+
+		// Вызов метода отрисовки данных игры
+		// game.rendering();
+
+		// Вызов игрового цикла
+		window.requestAnimationFrame(gameLoop);
+	}
+
+}
